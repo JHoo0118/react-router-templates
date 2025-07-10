c4a59902c93302b90dc7cefa28cf7032cc570615
JHoo0118 <kik3078@gmail.com>
2025-07-10 23:11:24 +0900

    feat: dependency 수정

--- e2e/i18n/i18n.server.test.ts
+++ e2e/i18n/i18n.server.test.ts
@@ -2,19 +2,19 @@ import { test, expect } from "@playwright/test";
 import remixI18n from "../../app/localization/i18n.server";
 
 test("Remix I18n - returns the correct default language from the request", async () => {
-  const request = new Request("http://localhost:3000");
+  const request = new Request("http://localhost:4280");
   const defaultLanguage = await remixI18n.getLocale(request);
   expect(defaultLanguage).toBe("ko");
 });
 
 test("Remix I18n - returns the correct default language from the request if search param lang is invalid", async () => {
-  const request = new Request("http://localhost:3000?lng=invalid");
+  const request = new Request("http://localhost:4280?lng=invalid");
   const defaultLanguage = await remixI18n.getLocale(request);
   expect(defaultLanguage).toBe("ko");
 });
 
 test("Remix I18n - returns the correct language when specified in the search params from the request", async () => {
-  const request = new Request("http://localhost:3000?lng=en");
+  const request = new Request("http://localhost:4280?lng=en");
   const defaultLanguage = await remixI18n.getLocale(request);
   expect(defaultLanguage).toBe("en");
 });
